4d3ceab2f6a6f185311aa5c1dceaeb97b6775f7f
Eriitunu Adesioye <eriitunuadesioye@yahoo.com>
2017-10-26 17:07:26 +0100

    Finished creating buttons for facebook and instagram login

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import {Text, View, ScrollView, TouchableHighlight, Image, StatusBar } from 'react-native';
 import Dimensions from 'Dimensions';
+import {LoginButton} from './src/components';
+
 
 const window = Dimensions.get('window');
 
+const standardComponentWidth = window.width * 0.82;
+
+const colors = {
+  facebook: 'rgb(59, 89, 152)',
+  text: 'rgba(255, 255, 255, 0.75)',
+  instagramButtonBorderColor: 'rgba( 255, 255, 255, 0.35)',
+  facebookButtonBorderColor: 'rgba( 255, 255, 255, 0.35)',
+
+}
+
+const loginButtonInfo = {
+  height: 45,
+  pageFontSize: 11,
+  borderWidth: 0.8,
+  borderRadius: 5,
+}
+
 export default class App extends React.Component {
+
+  loginButtonTapped = () => {
+    console.log('button was just tapped');
+  }
+
   render() {
     return (
       <Image source={require('./src/Images/insta_login_background.jpg')} style={viewStyles.container}>
@@ -15,8 +39,33 @@ export default class App extends React.Component {
           <Image source={require('./src/Images/instagram-text-logo.png')}
             resizeMode={'contain'}
             style={viewStyles.instagramTextLogo}
-
           />
+
+          <LoginButton
+          buttonViewStyle ={viewStyles.instagramLoginButtonView}
+          touchableHighlightStyle= {viewStyles.instagramLoginButtonHghlightStyle}
+          buttonTextStyle= {viewStyles.buttonTextStyle}
+          buttonTapped={this.loginButtonTapped}
+          activeOpacity={0.75}
+          >
+
+
+
+            Log  Into Erigram
+          </LoginButton>
+
+          <LoginButton
+          buttonViewStyle ={[viewStyles.instagramLoginButtonView, viewStyles.facebookLoginButtonView]}
+          touchableHighlightStyle= {viewStyles.facebookLoginButtonHighlightStyle}
+          buttonTextStyle= {viewStyles.buttonTextStyle}
+          buttonTapped={this.loginButtonTapped}
+          activeOpacity={0.75}
+          >
+
+
+
+            Facebook Login
+          </LoginButton>
         </ScrollView>
 
 
@@ -38,11 +87,50 @@ const viewStyles = {
     flex: 1,
     flexDirection: 'column'
   },
-  instagramTextLogo: {
-    width: 145,
-    height: 55,
-    marginTop: (window.height * 0.20),
-    marginBottom: 30,
 
+  instagramTextLogo: {
+    width: (window.height* 0.25),
+    height: (window.height* 0.12),
+    marginTop: (window.height * 0.15),
+    marginBottom: 30,
+    alignSelf:'center'
+  },
+
+  instagramLoginButtonView: {
+    backgroundColor: 'transparent',
+    borderColor: colors.instagramButtonBorderColor,
+    borderWidth: loginButtonInfo.borderWidth,
+    borderRadius: loginButtonInfo.borderRadius,
+    width: standardComponentWidth,
+    height: loginButtonInfo.height,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  facebookLoginButtonView: {
+    backgroundColor:colors.facebook,
+  },
+
+  instagramLoginButtonHghlightStyle: {
+    backgroundColor: 'transparent',
+    width: standardComponentWidth,
+    height: loginButtonInfo.height,
+    marginTop: 5,
+  },
+
+  facebookLoginButtonHighlightStyle: {
+    backgroundColor: colors.facebook,
+    width: standardComponentWidth,
+    height: loginButtonInfo.height,
+    marginTop: 20,
+    marginBottom: 10,
+    borderWidth: loginButtonInfo.borderWidth,
+    borderRadius: loginButtonInfo.borderRadius,
+    borderColor: loginButtonInfo.facebookButtonBorderColor
+  },
+
+  buttonTextStyle: {
+    color: colors.text
   }
+
 };
