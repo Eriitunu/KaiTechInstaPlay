b35e70171901b42a3705f8ffbafe489980547284
Eriitunu Adesioye <eriitunuadesioye@yahoo.com>
2017-10-26 14:21:58 +0100

    imported images to images folder that would make up the login screen

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, ScrollView, TouchableHighlight, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Finally We Can Start Our App</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const viewStyles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+};
