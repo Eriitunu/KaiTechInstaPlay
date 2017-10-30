28b673c93290ee3618a5dac65e1286e4a42114f8
Eriitunu Adesioye <eriitunuadesioye@yahoo.com>
2017-10-30 13:18:48 +0100

    imported Network Manager

---
+++ src/model/NetworkManager.js
@@ -0,0 +1,30 @@
+
+import axios from 'axios';
+
+
+const axiosEndpointManager = axios.create ({
+  baseURL: 'https://api.instagram.com/v1/users/',
+  timeout: 20000
+});
+
+class NetworkManager{
+
+  constructor(accessToken){
+    this.accessToken = accessToken
+  }
+
+  getLoggedInUserInformation(){
+    axiosEndpointManager.get('self/?access_token=' + this.accessToken)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(response => {
+      console.log("Ooops this is an error")
+      console.log(response)
+    });
+
+  }
+
+
+}
+export {NetworkManager};
